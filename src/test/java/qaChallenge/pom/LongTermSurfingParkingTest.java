0612ded3d3496cca5f2cb37432751812d9cab8ba
Yeser S <yeser07@gmail.com>
2021-10-06 17:53:02 -0600

    ending long term surface scenarios

--- src/test/java/qaChallenge/pom/LongTermSurfingParkingTest.java
+++ src/test/java/qaChallenge/pom/LongTermSurfingParkingTest.java
@@ -21,11 +21,11 @@ public class LongTermSurfingParkingTest {
 
 	@After
 	public void tearDown() throws Exception {
-		driver.quit();
+		//driver.quit();
 	}
 
 	@Test
-	public void longTermSurfaceParkingxHour() throws InterruptedException {
+	public void longTermSurfaceParkingxHourTest() throws InterruptedException {
 		baseLocator.clearForm();
 		baseLocator.dropDownListParkingLot("Long-Term Surface Parking");
 		baseLocator.inputTextValues("10/06/2021", "10/06/2021", "12:00","01:00");
@@ -35,5 +35,56 @@ public class LongTermSurfingParkingTest {
 		assertEquals(baseLocator.longTermSurfaceParking(0, 1),baseLocator.expectedResult());
 		Thread.sleep(4000);
 	}
+	
+	@Test
+	public void longTermSurfaceParkingxDayliMaximumTest() throws InterruptedException {
+		baseLocator.clearForm();
+		baseLocator.dropDownListParkingLot("Long-Term Surface Parking");
+		baseLocator.inputTextValues("10/06/2021", "10/06/2021", "12:00", "12:00");
+		baseLocator.setStartingTimeAM();
+		baseLocator.setLeavingTimePM();
+		baseLocator.summitButton();
+		assertEquals(baseLocator.longTermSurfaceParking(0,24),baseLocator.expectedResult());
+		Thread.sleep(4000);
+		
+	}
+	
+	@Test
+	public void longTermSurfaceParkingxWeekTest() throws InterruptedException {
+		baseLocator.clearForm();
+		baseLocator.dropDownListParkingLot("Long-Term Surface Parking");
+		baseLocator.inputTextValues("10/04/2021","10/11/2021", "12:00", "12:00");
+		baseLocator.setStartingTimeAM();
+		baseLocator.setLeavingTimeAM();
+		baseLocator.summitButton();
+		assertEquals(baseLocator.longTermSurfaceParking(7,0),baseLocator.expectedResult());
+		Thread.sleep(4000);
+	}
+	
+	@Test
+	public void longTermSurfaceMoreThanOneDayTest() throws InterruptedException {
+		baseLocator.clearForm();
+		baseLocator.dropDownListParkingLot("Long-Term Surface Parking");
+		baseLocator.inputTextValues("10/05/2021","10/07/2021" ,"12:00","1:00");
+		baseLocator.setStartingTimeAM();
+		baseLocator.setLeavingTimePM();
+		baseLocator.summitButton();
+		assertEquals(baseLocator.longTermSurfaceParking(2,13),baseLocator.expectedResult());
+		Thread.sleep(4000);
+	}
+	
+	@Test
+	public void longTermSurfaceMoreThanAWeekTest() throws InterruptedException {
+		baseLocator.clearForm();
+		baseLocator.dropDownListParkingLot("Long-Term Surface Parking");
+		baseLocator.inputTextValues("10/04/2021","10/20/2021","12:00", "08:00");
+		baseLocator.setStartingTimePM();
+		baseLocator.setLeavingTimePM();
+		baseLocator.summitButton();
+		assertEquals(baseLocator.longTermSurfaceParking(16,8),baseLocator.expectedResult());
+		Thread.sleep(4000);
+	}
+	
+	
 
 }
